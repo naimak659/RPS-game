fec4f0af54de9490629f7d9c8e0bb741509ca45c
Naim AK <naimak659@gmail.com>
2024-04-08 21:58:49 +0600

    fixed alot of things

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,35 @@
 let elementImages = [
-  { img: "/images/scissor.png", key: "Scissor" },
-  { img: "/images/paper.png", key: "Paper" },
-  { img: "/images/rock.png", key: "Rock" },
+  { img: "images/scissor.png", key: "Scissor" },
+  { img: "images/paper.png", key: "Paper" },
+  { img: "images/paper.png", key: "Paper" },
+  { img: "images/rock.png", key: "Rock" },
+  { img: "images/scissor.png", key: "Scissor" },
+  { img: "images/rock.png", key: "Rock" },
+  { img: "images/scissor.png", key: "Scissor" },
+  { img: "images/paper.png", key: "Paper" },
+  { img: "images/paper.png", key: "Paper" },
+  { img: "images/rock.png", key: "Rock" },
+  { img: "images/scissor.png", key: "Scissor" },
+  { img: "images/rock.png", key: "Rock" },
 ];
 
 const winnerCard = document.getElementById("card");
-const winner = document.querySelector(".winner p");
+const winLossCount = document.querySelector(".winLossCount ");
+const winner = document.querySelector(".winner ");
+const loss = document.querySelector(".loss ");
 const userResult = document.getElementById("user-img");
 const computerResult = document.getElementById("computer-img");
 const userCard = document.getElementById("user__selector");
-const userInput = document.querySelectorAll("#user-input img");
+const userInput = document.querySelectorAll("#user-input");
+const userInputImg = document.querySelectorAll("#user-input img");
 const computerResultName = document.querySelector(".computerResult_name");
 const userResultName = document.querySelector(".userResult_name");
+const restartBtn = document.querySelector(".restart__btn");
 
-userInput.forEach((input, i) => {
+let winCount = 0;
+let lossCount = 0;
+
+userInputImg.forEach((input, i) => {
   input.addEventListener("click", (e) => {
     clikedImg(input);
   });
@@ -23,10 +39,13 @@ function clikedImg(x) {
   let playerInput = x.getAttribute("src");
   let name = x.getAttribute("key");
   userResult.setAttribute("src", `${playerInput}`);
+  userResult.setAttribute("key", `${name}`);
+
   userResultName.textContent = name;
   computerInput();
-  //   userCard.classList.replace("grid", "hidden");
-  //   winnerCard.classList.replace("hidden", "grid");
+  // userCard.classList.replace("grid", "hidden");
+  winnerCard.classList.remove("hidden");
+  winnerCard.classList.add("grid");
 }
 
 // computer randomly selected img funtion
@@ -34,37 +53,40 @@ function computerInput() {
   let random = Math.floor(Math.random() * elementImages.length);
   let computerName = `${elementImages[random].key}`;
   computerResult.setAttribute("src", `${elementImages[random].img}`);
+  computerResult.setAttribute("key", `${elementImages[random].key}`);
   computerResultName.textContent = computerName;
   winnePlayer();
 }
 
 // winner
 function winnePlayer() {
-  let You = userResultName.textContent;
-  let Opponent = computerResultName.textContent;
-
+  let You = userResult.getAttribute("key");
+  let Opponent = computerResult.getAttribute("key");
+  // check win and loss count
   if (You == Opponent) {
-    winner.textContent = "It's a draw";
-  } else if ((You || Opponent == "Rock") && (You || Opponent == "Paper")) {
-    winner.textContent = `Winner is: ${You == "Paper" ? "You" : "Opponent"}`;
-    console.log(`Winner is: ${You == "Paper" ? "You" : "Opponent"}`);
-  } else if ((You || Opponent == "Scissor") && (You || Opponent == "Paper")) {
-    winner.textContent = `Winner is: ${You == "Scissor" ? "You" : "Opponent"}`;
-    console.log(`Winner is: ${You == "Scissor" ? `You` : `Opponent`}`);
-  } else if ((You || Opponent == "Scissor") && (You || Opponent == "Rock")) {
-    winner.textContent = `Winner is: ${You == "Rock" ? "You" : "Opponent"}`;
-    console.log(`Winner is: ${You == "Rock" ? "You" : "Opponent"}`);
+    return;
+  } else if (
+    (You == "Rock" && Opponent == "Paper") ||
+    (You == "Paper" && Opponent == "Scissor") ||
+    (You == "Scissor" && Opponent == "Rock")
+  ) {
+    lossCount = lossCount + 1;
+    return (loss.innerHTML = `loss: ${lossCount}`);
+  } else {
+    winCount = winCount + 1;
+    return (winner.innerHTML = `Win: ${winCount}`);
   }
-  // else if (player == "Rock" && Opponent == "Paper") {
-  //     winner.innerHTML = "Winner is: Opponent";
-  //   } else if (player == "Rock" && Opponent == "Scissor") {
-  //     winner.innerHTML = "Winner is: You";
-  //   } else if (player == "Paper" && Opponent == "Scissor") {
-  //     winner.innerHTML = "Winner is: Opponent";
-  //   } else if (player == "Paper" && Opponent == "Scissor") {
-  //     winner.innerHTML = "Winner is: You";
-  //   }
 }
+
+// restart
+restartBtn.addEventListener("click", () => {
+  winnerCard.classList.remove("grid");
+  winnerCard.classList.add("hidden");
+  winCount = 0;
+  lossCount = 0;
+  winner.innerHTML = `Win: ${winCount}`;
+  loss.innerHTML = `loss: ${lossCount}`;
+});
 
 // console.log(computerResult);
 // console.log(userResult);
